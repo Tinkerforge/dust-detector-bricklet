0bb5374e1e393c7d675adde00902cc87b6ff156d
Matthias Bolte <matthias@tinkerforge.com>
2015-08-12 11:16:00 +0200

    Fix minor warnings in C/C++ examples

--- software/examples/c/example_threshold.c
+++ software/examples/c/example_threshold.c
@@ -14,7 +14,7 @@ void cb_dust_density_reached(uint16_t dust_density, void *user_data) {
 	printf("Dust Density: %d µg/m³\n", dust_density);
 }
 
-int main() {
+int main(void) {
 	// Create IP connection
 	IPConnection ipcon;
 	ipcon_create(&ipcon);
@@ -26,7 +26,7 @@ int main() {
 	// Connect to brickd
 	if(ipcon_connect(&ipcon, HOST, PORT) < 0) {
 		fprintf(stderr, "Could not connect\n");
-		exit(1);
+		return 1;
 	}
 	// Don't use device before ipcon is connected
 
@@ -45,4 +45,5 @@ int main() {
 	printf("Press key to exit\n");
 	getchar();
 	ipcon_destroy(&ipcon); // Calls ipcon_disconnect internally
+	return 0;
 }
