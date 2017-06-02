ed0ec33392a7482fb52e8c1d15f80747fd951f6b
Matthias Bolte <matthias@tinkerforge.com>
2017-06-02 16:08:03 +0200

    octave: Use javaObject instead of deprecated java_new

--- software/examples/matlab/octave_example_callback.m
+++ software/examples/matlab/octave_example_callback.m
@@ -5,8 +5,8 @@ function octave_example_callback()
     PORT = 4223;
     UID = "XYZ"; % Change XYZ to the UID of your Dust Detector Bricklet
 
-    ipcon = java_new("com.tinkerforge.IPConnection"); % Create IP connection
-    dd = java_new("com.tinkerforge.BrickletDustDetector", UID, ipcon); % Create device object
+    ipcon = javaObject("com.tinkerforge.IPConnection"); % Create IP connection
+    dd = javaObject("com.tinkerforge.BrickletDustDetector", UID, ipcon); % Create device object
 
     ipcon.connect(HOST, PORT); % Connect to brickd
     % Don't use device before ipcon is connected
