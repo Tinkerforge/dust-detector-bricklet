c5bc40b14f383a59dde7378636809f8963b5f556
Erik Fleckstein <erik@tinkerforge.com>
2018-12-18 15:17:08 +0100

    go: Change import path again.

--- software/examples/go/example_callback.go
+++ software/examples/go/example_callback.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/tinkerforge/go-api-bindings/dust_detector_bricklet"
-	"github.com/tinkerforge/go-api-bindings/ipconnection"
+	"github.com/Tinkerforge/go-api-bindings/dust_detector_bricklet"
+	"github.com/Tinkerforge/go-api-bindings/ipconnection"
 )
 
 const ADDR string = "localhost:4223"
