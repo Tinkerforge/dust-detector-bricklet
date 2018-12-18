5f2ae689eaccb6399087846d7389ea412451ee44
Erik Fleckstein <erik@tinkerforge.com>
2018-12-18 14:53:06 +0100

    go: Change import path.

--- software/examples/go/example_threshold.go
+++ software/examples/go/example_threshold.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"tinkerforge/dust_detector_bricklet"
-	"tinkerforge/ipconnection"
+	"github.com/tinkerforge/go-api-bindings/dust_detector_bricklet"
+	"github.com/tinkerforge/go-api-bindings/ipconnection"
 )
 
 const ADDR string = "localhost:4223"
